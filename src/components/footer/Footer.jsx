0d7f80a1f94266439a03d758a2b52c05bdf40d6a
Vishal Mishra <vishalmishra@medianv.com>
2026-04-06 17:59:26 +0530

    updated footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import styles from'./footer.module.css'
-import Image from 'next/image'
+import React from "react";
+import styles from "./footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div>@2026 Shutpix.com. All rights reserved.!!</div>
+      <div>©2026 Shutpix.com. All rights reserved.</div>
       <div className={styles.social}>
-        <Image src="/1.png" width={15} height={15} className={styles.icon} alt="Vishal Mishra"/>
-        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="Vishal Mishra"/>
-        <Image src="/3.png" width={15} height={15} className={styles.icon} alt="Vishal Mishra"/>
-        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="Vishal Mishra"/>
-
+        <Image src="/1.png" width={15} height={15} className={styles.icon} alt="Lama Dev Facebook Account" />
+        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
+        <Image src="/3.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
+        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
