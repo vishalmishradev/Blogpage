45ceff0e62935e00253ddc9d223e524b393ea0fa
Vishal Mishra <vishalmishra@medianv.com>
2026-04-07 14:22:09 +0530

    completed portfolio page

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
+import styles from './page.module.css'
+import Button from '@/components/Button/Button';
+import Image from 'next/image';
 
-const Category = () => {
+const Category = async({params}) => {
+  const val = await params;
   return (
-    <div>
-      Category
+    <div className={styles.container}>
+      <h1 className={styles.catTitle} >{val.category}</h1>
+      <div className={styles.item} >
+      <div className={styles.content}>
+        <h1 className={styles.title}>Test</h1>
+        <p className={styles.desc}>Desc</p>
+        <Button text="see more" url="#"/>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image
+        className={styles.img}
+        fill={true}
+        src="https://images.pexels.com/photos/34870427/pexels-photo-34870427.jpeg"
+        alt=""
+        />
+        </div>
+      </div>
+
+      <div className={styles.item} >
+      <div className={styles.content}>
+        <h1 className={styles.title}>Test</h1>
+        <p className={styles.desc}>Desc</p>
+        <Button text="see more" url="#"/>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image
+        className={styles.img}
+        fill={true}
+        src="https://images.pexels.com/photos/34870427/pexels-photo-34870427.jpeg"
+        alt=""
+        />
+        </div>
+      </div>
+
+      <div className={styles.item} >
+      <div className={styles.content}>
+        <h1 className={styles.title}>Test</h1>
+        <p className={styles.desc}>Desc</p>
+        <Button text="see more" url="#"/>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image
+        className={styles.img}
+        fill={true}
+        src="https://images.pexels.com/photos/34870427/pexels-photo-34870427.jpeg"
+        alt=""
+        />
+        </div>
+      </div>
     </div>
   )
 }
