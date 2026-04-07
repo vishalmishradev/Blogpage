4a9e721ae3565dd90135fc00215ec57bd279cdc1
Vishal Mishra <vishalmishra@medianv.com>
2026-04-07 12:34:26 +0530

    added portfolio to our web

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import styles from './page.module.css'
 
 const Category = () => {
   return (
-    <div className={styles.container}>
+    <div>
       Category
     </div>
   )
