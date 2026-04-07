c2c55dfc55e5b9f6daa983da962380672ac4053b
Vishal Mishra <vishalmishra@medianv.com>
2026-04-07 15:49:00 +0530

    added blog and respective id pages

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
 
-const page = () => {
+
+
+const BlogPost = () => {
   return (
-    <div>
-      Blog page
-    </div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>
+            This is a demo title for my blog page
+          </h1>
+          <p className={styles.desc}>
+            This is a demo description of my web page.This is a demo description of my web page.This is a demo description of my web page.This is a demo description of my web page.This is a demo description of my web page.This is a demo description of my web page.This is a demo description of my web page.This is a demo description of my web page.This is a demo description of my web page.This is a demo description of my web page.This is a demo description of my web page.This is a demo description of my web page.This is a demo description of my web page.This is a demo description of my web page.This is a demo description of my web page.This is a demo description of my web page.This is a demo description of my web page.This is a demo description of my web page.
+          </p>
+          <div className={styles.author}>
+           <Image
+              src="https://images.pexels.com/photos/34870427/pexels-photo-34870427.jpeg"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>Vishal Mishra</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+              src="https://images.pexels.com/photos/34870427/pexels-photo-34870427.jpeg"
+              alt=""
+              fill={true}
+              className={styles.image}
+            />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+         This is a demo description of my web page.
+        This is a demo description of my web page.
+         This is a demo description of my web page.
+         This is a demo description of my web page.
+         This is a demo description of my web page.
+         This is a demo description of my web page.
+         This is a demo description of my web page.
+         This is a demo description of my web page.
+         This is a demo description of my web page.
 
-export default page
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default BlogPost;
