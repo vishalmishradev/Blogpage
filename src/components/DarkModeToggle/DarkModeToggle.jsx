cecf9043da46e59504a1762e222ce645ab0b9d4c
Vishal Mishra <vishalmishra@medianv.com>
2026-04-07 18:21:15 +0530

    added usecontext for dark and light mode

--- src/components/DarkModeToggle/DarkModeToggle.jsx
+++ src/components/DarkModeToggle/DarkModeToggle.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import styles from './darkModeToggle.module.css'
+"use client"
+
+import React, { useContext } from "react";
+import styles from "./darkModeToggle.module.css";
+import { ThemeContext } from "../../context/ThemeContext";
 
 const DarkModeToggle = () => {
-    const mode ="dark";
+  const { toggle,mode } = useContext(ThemeContext);
   return (
-    <div className={styles.container}>
-        <div className={styles.icon}>🌙</div>
-        <div className={styles.icon}>☀️</div>
-        <div className={styles.ball} style={mode==="light" ? {left:"2px"} : {right:"2px"}}>
-        </div>
+    <div className={styles.container} onClick={toggle}>
+      <div className={styles.icon}>🌙</div>
+      <div className={styles.icon}>🔆</div>
+      <div
+        className={styles.ball}
+        style={mode === "light" ? { left: "2px" } : { right: "2px" }}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default DarkModeToggle
+export default DarkModeToggle;
